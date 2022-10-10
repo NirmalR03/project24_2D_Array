2f074d308c23fe8f2d2cc2daa98629f77200809a
NirmalR03 <72380160+NirmalR03@users.noreply.github.com>
2022-10-10 13:10:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const row = prompt("Enter no of Rows"), column =prompt("Enter no of  Column");
 var arr = [];
-for (var i=0;i<row;i++) {
+for (var i=0;i<row;i++)
+{
 	arr[i] = [];
-
-	for (var j=0;j<column;j++) {
+	for (var j=0;j<column;j++) 
+	{
 		arr[i][j] = prompt(` Enter Value for Arrays ${row} x ${column}`);
 	}
 }
