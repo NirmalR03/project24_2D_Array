c68768e4e776b9a6ef4db1c8d9481dd731efe548
NirmalR03 <72380160+NirmalR03@users.noreply.github.com>
2022-10-10 12:53:08 +0530

    commit

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const row = 3, column = 4;
+const row = prompt("Enter Rows), column =prompt("Enter Column");
 var arr = [];
 for (var i=0;i<row;i++) {
 	arr[i] = [];
 
 	for (var j=0;j<column;j++) {
-		arr[i][j] = prompt("Value for Array");
+		arr[i][j] = prompt(` Enter Value for Array ${row} x ${column}`);
 	}
 }
 console.log(arr);
